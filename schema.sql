03c199e18b844e7f144b2e402007ca3281a3fd3e
luftedar <ugurorcun91@gmail.com>
2022-01-11 13:58:29 +0300

    Create Table / Copied Table into schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,50 @@
-/* Database schema to keep the structure of entire database. */
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE animals (
-    name varchar(100)
+-- Dumped from database version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+-- Dumped by pg_dump version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.animals (
+    id integer,
+    name character varying(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric
 );
+
+
+ALTER TABLE public.animals OWNER TO postgres;
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
