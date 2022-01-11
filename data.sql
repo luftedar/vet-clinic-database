51766ece6affcca33507c952f85f9bfd67a9fa10
luftedar <ugurorcun91@gmail.com>
2022-01-11 14:51:51 +0300

    Add Required Queries

--- data.sql
+++ data.sql
@@ -1,5 +1,144 @@
-/* Populate database with sample data. */
+-- Please Note Here. Lines Between 3 to 83 are output of the postgresql for the requirements.
+-- You can see the queries after line 83.
+-- PostgreSQL database dump
+--
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+-- Dumped from database version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+-- Dumped by pg_dump version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.animals (
+    id integer NOT NULL,
+    name character varying(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric
+);
+
+
+ALTER TABLE public.animals OWNER TO postgres;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) FROM stdin;
+2	Agumon		2020-02-03	0	t	10.23
+3	Gabumon	2018-11-15	2	t	8
+4	Pikachu	2021-01-07	1	f	15.04
+5	Devimon	2017-05-12	5	t	11
+\.
+
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.animals_id_seq', 5, true);
+
+
+--
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.animals
+    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+--Animal: His name is Agumon. He was born on Feb 3rd, 2020, and currently weighs 10.23kg. He was neutered and he has never tried to escape.
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+    ) VALUES (
+        'Agumon',
+        '2020-2-3',
+        0,
+        true,
+        10.23
+        );
+
+--Animal: Her name is Gabumon. She was born on Nov 15th, 2018, and currently weighs 8kg. She is neutered and she has tried to escape 2 times.
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+    ) VALUES (
+        'Gabumon',
+        '2018-11-15',
+        2,
+        true,
+        8
+        );
+
+--Animal: His name is Pikachu. He was born on Jan 7th, 2021, and currently weighs 15.04kg. He was not neutered and he has tried to escape once.
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+    ) VALUES (
+        'Pikachu',
+        '2021-1-7',
+        1,
+        false,
+        15.04
+        );
+
+--Animal: Her name is Devimon. She was born on May 12th, 2017, and currently weighs 11kg. She is neutered and she has tried to escape 5 times.
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+    ) VALUES (
+        'Devimon',
+        '2017-5-12',
+        5,
+        true,
+        11
+        );
--- queries.sql
+++ queries.sql
@@ -1,3 +1,159 @@
-/*Queries that provide answers to the questions from all projects.*/
+-- Please Note Here. Lines Between 3 to 83 are output of the postgresql for the requirements.
+-- You can see the queries after line 83.
+-- PostgreSQL database dump
+--
 
-SELECT * from animals WHERE name = 'Luna';
+-- Dumped from database version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+-- Dumped by pg_dump version 14.1 (Ubuntu 14.1-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.animals (
+    id integer NOT NULL,
+    name character varying(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric
+);
+
+
+ALTER TABLE public.animals OWNER TO postgres;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) FROM stdin;
+2	Agumon		2020-02-03	0	t	10.23
+3	Gabumon	2018-11-15	2	t	8
+4	Pikachu	2021-01-07	1	f	15.04
+5	Devimon	2017-05-12	5	t	11
+\.
+
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.animals_id_seq', 5, true);
+
+
+--
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.animals
+    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--Find all animals whose name ends in "mon".
+SELECT * FROM animals WHERE name LIKE '%mon';
+-- id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+----+---------+---------------+-----------------+----------+-----------
+--  2 | Agumon  | 2020-02-03    |               0 | t        |     10.23
+--  3 | Gabumon | 2018-11-15    |               2 | t        |         8
+--  5 | Devimon | 2017-05-12    |               5 | t        |        11
+--(3 rows)
+
+
+--List the name of all animals born between 2016 and 2019.
+SELECT name FROM animals WHERE date_of_birth BETWEEN '1-1-2016' AND '1-1-2019';
+--  name   
+-----------
+-- Gabumon
+-- Devimon
+--(2 rows)
+
+
+
+--List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name FROM animals WHERE neutered=true AND escape_attempts<3;
+--  name   
+---------
+-- Agumon
+-- Gabumon
+--(2 rows)
+
+--List date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
+-- date_of_birth 
+---------------
+-- 2020-02-03
+-- 2021-01-07
+--(2 rows)
+
+
+--List name and escape attempts of animals that weigh more than 10.5kg
+vet-clinic=# SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
+--  name   | escape_attempts 
+---------+-----------------
+-- Pikachu |               1
+-- Devimon |               5
+--(2 rows)
+
+
+--Find all animals that are neutered.
+vet-clinic=# SELECT * FROM animals WHERE neutered = true;
+-- id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+----+---------+---------------+-----------------+----------+-----------
+--  2 | Agumon  | 2020-02-03    |               0 | t        |     10.23
+--  3 | Gabumon | 2018-11-15    |               2 | t        |         8
+--  5 | Devimon | 2017-05-12    |               5 | t        |        11
+--(3 rows)
+
+
+--Find all animals not named Gabumon.
+
+vet-clinic=# SELECT * FROM animals WHERE NOT name = 'Gabumon';
+-- id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+----+---------+---------------+-----------------+----------+-----------
+--  2 | Agumon  | 2020-02-03    |               0 | t        |     10.23
+--  4 | Pikachu | 2021-01-07    |               1 | f        |     15.04
+--  5 | Devimon | 2017-05-12    |               5 | t        |        11
+--(3 rows)
+
+
+--Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights --that equals precisely 10.4kg or 17.3kg)
+vet-clinic=# SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+-- id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+----+---------+---------------+-----------------+----------+-----------
+--  4 | Pikachu | 2021-01-07    |               1 | f        |     15.04
+--  5 | Devimon | 2017-05-12    |               5 | t        |        11
+--(2 rows)
+
